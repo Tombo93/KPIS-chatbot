715219653a92bfa33ee1953d7e9cd57dd33f69c6
Tom <tomliessmann@gmail.com>
2025-11-18 13:12:05 +0100

    Added fictional data and filter by tag function for it

--- src_v1/agent.py
+++ src_v1/agent.py
@@ -3,18 +3,33 @@ import pandas as pd
 from dotenv import load_dotenv
 from langchain.agents import create_agent
 
+########## Helpers ##########
+def filter_df_by_col(df, col, search_string):
+    df = df[df[col].str.contains(search_string)]
+    return df
+# ------------------------- #
 
+
+########## Configuration ##########
 load_dotenv()
 API_KEY_PRESSEPORTAL = os.getenv("API_KEY_PRESSEPORTAL")
 API_KEY_LLM = os.getenv("API_KEY_OPENAI")
+# ------------------------------- #
 
-prompts = pd.read_csv("prompts.csv")
 
-agent = create_agent(
-    model="gpt-4o",
-    tools=[],
-    system_prompt="You are a helpful assistant",
-)
+########## Data ##########
+data = pd.read_csv("artikel-fiktiv.csv")
+filtered_data = filter_df_by_col(data, "tags", "Polizei")
+print(filtered_data)
+# ------------------------------- #
 
-# Run the agent
-agent.invoke({"messages": [{"role": "user", "content": "what is the weather in sf"}]})
+
+# prompts = pd.read_csv("prompts.csv")
+# agent = create_agent(
+#     model="gpt-4o",
+#     tools=[],
+#     system_prompt="You are a helpful assistant",
+# )
+
+# # Run the agent
+# agent.invoke({"messages": [{"role": "user", "content": "what is the weather in sf"}]})
