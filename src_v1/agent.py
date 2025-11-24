f79f4fd7607d9bea1009652f4f46462bbe2ce59a
Tom <tomliessmann@gmail.com>
2025-11-24 12:26:14 +0100

    Agent development 1

--- src_v1/agent.py
+++ src_v1/agent.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
-# from langchain.agents import create_agent
+from langchain.agents import create_agent
 
 ########## Helpers ##########
 def filter_df_by_col(df, col, search_string):
@@ -22,20 +22,25 @@ os.environ["OPENAI_API_KEY"] = os.getenv("API_KEY_OPENAI")
 data = pd.read_csv("artikel-fiktiv.csv")
 filtered_data = filter_df_by_col(data, "tags", "Polizei")
 # print(filtered_data)
+prompts = pd.read_csv("prompts.csv")
 # ------------------------------- #
 
 
-prompts = pd.read_csv("prompts.csv")
-
+########## Agent Definition ##########
 model = ChatOpenAI(model="gpt-5-nano")
-response = model.invoke("Why do parrots talk?")
+
+SYSTEM_PROMPT = "Du bist ein neutraler Berichterstatter"
+
+agent = create_agent(
+    model=model,
+    tools=[],
+    system_prompt=SYSTEM_PROMPT,
+)
+# ---------------------------------- #
+
+
+########## Experiment Definition ##########
+response = agent.invoke({"messages": [{"role": "user", "content": "Generiere einen API-Call ans deutsche Presseportal mit Stories zu Berlin"}]})
+
 print(response)
-
-# agent = create_agent(
-#     model=model,
-#     tools=[],
-#     system_prompt="You are a helpful assistant",
-# )
-
-# # Run the agent
-# agent.invoke({"messages": [{"role": "user", "content": "what is the weather in sf"}]})
+# --------------------------------------- #
